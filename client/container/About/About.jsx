446b91869cb8c77ed99613c8899fcf2d7a91c82e
Insomnia <jamie.mar.jones14@gmail.com>
2020-11-03 15:23:31 -0500

    added the react html to the About page and the css to the About.css. Also added the background photo

---
+++ client/container/About/About.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import "./About.css";
+
+const About = () => {
+    return(
+        <>
+        <main className="content">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-8">
+            <article className="jumbotron background">
+              <h1 className="display-4">About Me</h1>
+              <hr className="my-4" />
+              <section className="row">
+                <!--Putting the paragraph and the image in the same column, and making the thumbnail float left allows the paragraph to frame the two available sides-->
+                <section className="col-sm-12">
+                  <img
+                    className="img img-thumbnail"
+                    id="float-left"
+                    src="./assets/about-me.png"
+                    alt="Photo of Jamie Jones"
+                    width="350"
+                    height="350"
+                  />
+                  <p>
+                    Hi! My name is Jamie Jones! I am new to web development,
+                    beginning my career as a student in an online Bootcamp for
+                    web development. Before this, I was in the Army for a couple
+                    of years until I was medically discharged and returned home
+                    to Georgia. For a year, I worked at an animal hospital as a
+                    veterinary and kennel technician with the intention of
+                    getting experience before going to school to become a
+                    veterinarian. It wasn't long before I realized my true
+                    desire in life is to travel the world, and from a friend, I
+                    heard about web development, which would give me that
+                    possibility while I still worked. After research, I decided
+                    this was something I would find enjoyable, and I quickly
+                    signed up for a three month web development course.
+                  </p>
+                </section>
+              </section>
+              <section className="row">
+                <section className="col-sm-12">
+                  <p>
+                    I am an animal lover and adopted a three week old kitten,
+                    named Ellie, while working at the animal hospital. I also
+                    have a 12 year old dog named Chanel. I love horror movies
+                    and listening to KPop, as well as playing video games, and
+                    reading and writing. I love camping and kayaking and can't
+                    wait to travel the world.
+                  </p>
+                </section>
+              </section>
+            </article>
+          </div>
+        </div>
+      </div>
+    </main>
+        </>
+    )
+}
